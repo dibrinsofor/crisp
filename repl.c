63905004fb2687e31d9755ea50a8ef52d6d4d427
Dibri Nsofor <dibrinsofor@gmail.com>
2023-06-06 13:23:19 +0100

    crisp update: ast gen

--- repl.c
+++ repl.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "mpc.h"
 
 #ifdef _WIN32
 #include <string.h>
@@ -30,6 +31,20 @@ void add_history(char * unused) {}
  
 int main(int argc, char** argv) {
 
+    mpc_parser_t * Number = mpc_new("number");
+    mpc_parser_t * Operator = mpc_new("operator");
+    mpc_parser_t * Expr = mpc_new("expr");
+    mpc_parser_t * Crisp = mpc_new("crisp");
+
+    mpca_lang(MPCA_LANG_DEFAULT,
+    "                                                     \
+        number   : /-?[0-9]+/ ;                             \
+        operator : '+' | '-' | '*' | '/' ;                  \
+        expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+        crisp    : /^/ <operator> <expr>+ /$/ ;             \
+    ",
+    Number, Operator, Expr, Crisp);
+
     puts("Crisp Version 0.0.0.0.1\n");
     puts("Press Ctrl+c to Exit\n");
 
@@ -39,10 +54,21 @@ int main(int argc, char** argv) {
         char *input = readline(prompt);
         add_history(input);
 
-        // dummy output
-        printf("I do not understand: %s\n", input);
+        // parse input
+        mpc_result_t r;
+        if (mpc_parse("<stdin>", input, Crisp, &r)) {
+            mpc_ast_print(r.output);
+            mpc_ast_delete(r.output);
+        } else {
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
+
+        // printf("I do not understand: %s\n", input);
         free(input);
     }
+
+    mpc_cleanup(4, Number, Operator, Expr, Crisp);
 
     return 0;
 }
